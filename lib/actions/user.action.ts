2889965a0336e23c2ee530cb981f341c919e1747
KiranPK12 <kiranpadma2003@gmail.com>
2024-01-03 16:41:22 +0530

    first commit

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,8 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import { revalidatePath } from "next/cache";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -31,16 +33,27 @@ export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
   }
 };
 
-// delete user
 
 export const deleteUser = async (clerkId: string) => {
   try {
     await connectToDatabase();
-    const toDeleteUser = await User.findOne({ clerkId });
+    const userToDelete = await User.findOne({ clerkId });
 
-    if (!toDeleteUser) throw new Error("User not Found");
+    if (!userToDelete) throw new Error("User not Found");
+    await Promise.all([
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
 
-    const deletedUser = await User.findByIdAndDelete(toDeleteUser._id);
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
     revalidatePath("/");
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
